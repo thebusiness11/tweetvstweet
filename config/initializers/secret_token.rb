3ee30bd8a949dbb04254e97f0c88dddecd872187
Lance Velasco <lance@mlvgroup.com>
2012-11-25 23:56:20 -0800

    Devise Setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tweetvstweet::Application.config.secret_token = '3406bc76020f8ee3af5e9a857e3ac952722a790022b2ffa8d8c140d16ed405f20cadf9a04a908ef2b59c493518d19c3a9ebc74dd2cbfe14a8abbeb3fdd0fbb47'
