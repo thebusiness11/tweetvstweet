611bdb21611c99499e2d906ca4ffb01d1a3a3fbf
Lance Velasco <lance@mlvgroup.com>
2012-11-26 19:59:10 -0800

    fixed callbacks

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -2,9 +2,9 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     def all
     	user = User.from_omniauth(request.env["omniauth.auth"])
     	if user.persisted?
-    		flash.notice = "Signed in with twitter"
     		sign_in_and_redirect user, notice: "TaDa"
     	else
+            session["devise.user_attributes"] = user.attributes
     		redirect_to new_user_registration_url    		
     	end
 		end
