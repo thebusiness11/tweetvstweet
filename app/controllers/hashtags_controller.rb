683eba87566bec04e45de8fb2ecba3fde69f8f32
Lance Velasco <lance@mlvgroup.com>
2012-12-15 08:30:19 -0800

    refactored

--- app/controllers/hashtags_controller.rb
+++ app/controllers/hashtags_controller.rb
@@ -1,42 +1,40 @@
 class HashtagsController < ApplicationController
 
-	def cast_vote  											#Voting in the middle of the page
- 	Hashtag.cast_vote(params[:cast_vote])					#Tells which hashtag to vote for
-	@vote_history = Hashlog.vote_history					#updates the vote history on the bottom of the page
-	@leaderboard = Hashtag.leaderboard_history_current 		#updates the leaderboard on the right
- 		@cast_vote_hashtag = Hashtag.cast_vote_hashtag(params[:hashtag])
- 		respond_to do |format|
- 		format.html { redirect_to root_path }
- 		format.js
-		end
-	end	
-
-
+	before_filter :stats
 
 	def home	
-		@vote_history = Hashlog.vote_history
 		@leaderboard = Hashtag.leaderboard_history
-		# @trends_pull = Trend.trends_pull
 		@trends_display = Trend.trends_display
 	end
-	
+
 	def create 
-		@stats_total = Hashtag.count 
-		@stats_wins = Hashtag.sum(:wins)
-		@stats_views = Hashtag.sum(:view_count)
-		@stats_losers = (@stats_views - @stats_wins) 
-		@vote_history = Hashlog.vote_history
-		if signed_in?
- 			Hashtag.create_hashtag_signed_in(params[:hashtag])
- 		else
- 			Hashtag.create_hashtag_guest(params[:hashtag])
- 		end
- 		Hashlog.create_hashlog(params[:hashtag])
+		@hash_create = Hashtag.create_hashtag(params[:hashtag])
+		Hashlog.create_hashlog(params[:hashtag])
 		@random_hashtag_pull = Hashtag.random_hashtags_pull
 		@leaderboard = Hashtag.leaderboard_history_current
 		respond_to do |format|
 			format.html { redirect_to root_path }
-			format.js { render :flash=>'alert("Cant have a blank tweet bro");' }
+			format.js 
 		end
 	end
+
+	def cast_vote  											#Voting in the middle of the page
+	 	Hashtag.cast_vote(params[:cast_vote])					#Tells which hashtag to vote for
+		@leaderboard = Hashtag.leaderboard_history_current 		#updates the leaderboard on the right
+ 		@cast_vote_hashtag = Hashtag.cast_vote_hashtag(params[:hashtag])
+ 		respond_to do |format|
+	 		format.html { redirect_to root_path }
+	 		format.js
+		end
+	end	
+
+	private
+	  def stats
+			@stats_total = Hashtag.count 
+			@stats_wins = Hashtag.sum(:wins)
+			@stats_views = Hashtag.sum(:view_count)
+			@stats_losers = (@stats_views - @stats_wins) 
+			@vote_history = Hashlog.vote_history
+		end
+
 end
--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -26,32 +26,13 @@ class Hashtag < ActiveRecord::Base
                     :format   => { :with => hashtag_regex }
   
 
+ class << self
 
-  def self.create_hashtag_signed_in(hashtag)            #creates new tweetvstweet for inputed hashtag with # for logged in users
+def create_hashtag(hashtag)                #creates new tweetvstweet for inputed hashtag with # for guests
     dash = "#"
-    @hashtag_scrubbed = [dash, hashtag].join
-    User.current_user.twitter.search("%#{@hashtag_scrubbed}", :lang => "en", :count => 100, :result_type => "mixed").results.map do |tweet|
-  	unless exists?(tweet_id: tweet.id)
-  		create!(
-  			tweet_id: tweet.id,
-  			text: tweet.text,
-  			profile_image_url: tweet.user.profile_image_url,
-  			from_user: tweet.from_user,
-        from_user_name: tweet.user.name,
-   			created_at: tweet.created_at,
-        hashtag: @hashtag_scrubbed,
-        view_count: "0",
-        wins: "0"
-          )	
-  		end		
-  	end
-  end
-
-  def self.create_hashtag_guest(hashtag)                #creates new tweetvstweet for inputed hashtag with # for guests
-  dash = "#"
-  @hashtag_scrubbed = [dash, hashtag].join.downcase
-  Twitter.search("%#{@hashtag_scrubbed}", :lang => "en", :count => 100, :result_type => "mixed").results.map do |tweet|
-    unless exists?(tweet_id: tweet.id)
+    @hashtag_scrubbed = [dash, hashtag].join.downcase
+    (User.current_user ? User.current_user.twitter : Twitter).search("%#{@hashtag_scrubbed}", :lang => "en", :count => 100, :result_type => "mixed").results.map do |tweet|
+        unless exists?(tweet_id: tweet.id)
       create!(
         tweet_id: tweet.id,
         text: tweet.text,
@@ -63,34 +44,36 @@ class Hashtag < ActiveRecord::Base
         view_count: "0",
         wins: "0"
           ) 
-      end   
+        end  
     end
-  end
+end
+ 
 
 
-  def self.random_hashtags_pull                         #pulls 4 random hashtags for a vote first display
+  def random_hashtags_pull                         #pulls 4 random hashtags for a vote first display
     Hashtag.where{ |hashtag| hashtag.hashtag =~ @hashtag_scrubbed}.order{"RANDOM()"}.limit(4).each(&:update_view_count)
   end
 
-  def self.cast_vote_hashtag(hashtag)                    #pulls 4 random hashtags for a vote continued votes
+  def cast_vote_hashtag(hashtag)                    #pulls 4 random hashtags for a vote continued votes
     Hashtag.where{ |hashtag| hashtag.hashtag =~ @hashtag_scrubbed}.order{"RANDOM()"}.limit(4).each(&:update_view_count)
   end
 
-  def self.leaderboard_history_current                  #displaying 5 highscore hashtags on the right
+  def leaderboard_history_current                  #displaying 5 highscore hashtags on the right
     Hashtag.where{ |hashtag| hashtag.hashtag =~ @hashtag_scrubbed}.order{"wins DESC"}.limit(5)  
   end
+
+  def cast_vote(cast_vote)                           #counts number of wins
+    Hashtag.increment_counter(:wins, cast_vote)
+  end
+
+  def leaderboard_history                            #right side leaderboard history
+    Hashtag.order('wins DESC').limit(5)
+  end
+end
 
   def update_view_count                                  #updates the number of views
     Hashtag.increment_counter(:view_count, self.id)
     self.view_count += 1 
-  end
-
-  def self.cast_vote(cast_vote)                           #counts number of wins
-    Hashtag.increment_counter(:wins, cast_vote)
-  end
-
-  def self.leaderboard_history                            #right side leaderboard history
-    Hashtag.order('wins DESC').limit(5)
   end
 end
 
