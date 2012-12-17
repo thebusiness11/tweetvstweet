0745de1f3d98817239cfe0c617ca78d89fb2c1a8
Lance Velasco <lance@mlvgroup.com>
2012-12-16 16:01:16 -0800

    %% to search

--- app/controllers/autocomplete_controller.rb
+++ app/controllers/autocomplete_controller.rb
@@ -1,7 +1,8 @@
 class AutocompleteController < ApplicationController
 
 	def index
-		@autocomplete_data = Hashlog.order(:tag).where("tag like?", "%#{params[:term]}")
+		# @autocomplete_data = Hashlog.order(:tag).where("tag ilike?", "#{params[:term]}:*")
+		@autocomplete_data = Hashlog.where{ |hashlog| hashlog.tag =~ "%#{params[:term]}%"}
 		render json: @autocomplete_data.map(&:tag)
 	end
 end
